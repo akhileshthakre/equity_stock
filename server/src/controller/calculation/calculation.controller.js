45f299ffcf62fa464bccd18f2c82f258b2f597a6
Akhilesh Thakre <akhilesh@Mac.local>
2024-01-07 13:10:04 +0530

    [07-Jan] Handle multiple file in backend

--- server/src/controller/calculation/calculation.controller.js
+++ server/src/controller/calculation/calculation.controller.js
@@ -6,7 +6,7 @@ const calculateValues = require('./_helpers/calculateValues');
 
 const NUM_THREADS = 4;
 
-const fetchDataBatch = async (stocks, testValues, slossPercent, tgPercent, tsPercent) => {
+const fetchDataBatch = async (stocks, testValues, slossPercent, tgPercent, tsPercent, stockId) => {
   const resp = await Promise.all(
     testValues.map(async (testValue) => {
       const result = calculateValues(stocks, testValue.dataValues, slossPercent, tgPercent, tsPercent);
@@ -21,7 +21,7 @@ const fetchDataBatch = async (stocks, testValues, slossPercent, tgPercent, tsPer
       const winPercent = numberOfUpMoves > 0 ? (numberOfUpMoves / totalDays) * 100 : 0;
 
       return {
-        nameOfStock: stocks[1].dataValues.name,
+        nameOfStock: stockId || stocks[0].dataValues.name,
         fallInStock: testValue.dataValues.fallInStock,
         limitLevel: testValue.dataValues.limitLevel,
         hldDay: testValue.dataValues.hldDay,
@@ -40,33 +40,55 @@ const fetchDataBatch = async (stocks, testValues, slossPercent, tgPercent, tsPer
 
 const CalculationService = {
   fetchData: async (req, res) => {
-    const { page = 1, pageSize = 100, downloadAll = false, slossPercent, tgPercent, tsPercent  } = req.body;
-    let stocks, testValues;
+    const { page = 1, pageSize = 100, downloadAll = false, slossPercent, tgPercent, tsPercent, stockId  } = req.body;
+    let stocks, testValues, filteredTestValues, results;
 
     try {
       if (downloadAll) {
         [stocks, testValues] = await Promise.all([Stock.findAll(), TestValue.findAll()]);
-      } else {
+      } else if(stockId){
+        const offset = (page - 1) * pageSize;
+        testValues = await TestValue.findAll({ offset, limit: pageSize });
+        stocks = await Stock.findAll({
+          where: {
+            name: stockId,
+          },
+        });
+      }else {
         const offset = (page - 1) * pageSize;
         testValues = await TestValue.findAll({ offset, limit: pageSize });
         stocks = await Stock.findAll();
       }
-
+      const whereClause = stockId ? { name: stockId }: null
+      const uniqueStocks = await Stock.findAll({
+        where: {...whereClause},
+        attributes: ['name'],
+        group: ['name'],
+        raw: true,
+      });      
       const testValuesBatches = Array.from({ length: NUM_THREADS }, (_, index) =>
         testValues.slice((index * testValues.length) / NUM_THREADS, ((index + 1) * testValues.length) / NUM_THREADS)
       );
 
-      const workerPromises = testValuesBatches.map((batch) =>
-        new Promise((resolve) => {
-          const worker = new Worker(__filename, {
-            workerData: { stocks, testValues: batch, slossPercent, tgPercent, tsPercent},
-          });
+      results = [];
 
-          worker.on('message', resolve);
-        })
-      );
+      for (const stock of uniqueStocks) {
+        const stockName = stock.name;
+        filteredTestValues = stocks.filter(s => s.name === stockName);
 
-      const results = await Promise.all(workerPromises);
+        const workerPromises = testValuesBatches.map((batch) =>
+          new Promise((resolve) => {
+            const worker = new Worker(__filename, {
+              workerData: { filteredTestValues, testValues: batch, slossPercent, tgPercent, tsPercent, stockId},
+            });
+
+            worker.on('message', resolve);
+          })
+        );
+
+        const stockResults = await Promise.all(workerPromises);
+        results.push(stockResults);
+      }
 
       const resp = results.flat();
 
@@ -80,8 +102,8 @@ const CalculationService = {
 };
 
 if (!isMainThread) {
-  const { stocks, testValues, slossPercent, tgPercent, tsPercent } = workerData;
-  fetchDataBatch(stocks, testValues, slossPercent, tgPercent, tsPercent).then((result) => {
+  const { filteredTestValues, testValues, slossPercent, tgPercent, tsPercent, stockId } = workerData;
+  fetchDataBatch(filteredTestValues, testValues, slossPercent, tgPercent, tsPercent, stockId).then((result) => {
     parentPort.postMessage(result);
   });
 } else {
