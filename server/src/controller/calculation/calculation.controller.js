e9b4787dcb2141131c6b07984136af49cbee0fb2
Akhilesh Thakre <akhilesh@Mac.local>
2024-02-03 21:25:08 +0530

    [Feb-3] fix the data correction issue

--- server/src/controller/calculation/calculation.controller.js
+++ server/src/controller/calculation/calculation.controller.js
@@ -13,7 +13,7 @@ const fetchDataBatch = async (stocks, testValues, slossPercent, tgPercent, tsPer
       const result = calculateValues(stocks, testValue.dataValues, slossPercent, tgPercent, tsPercent);
       const numberOfUpMoves = result.filter((stock) => stock.Ret > 0.0).length;
       const numberOfDownMoves = result.filter((stock) => stock.Ret === 0.0 || stock.Ret < 0.0).length;
-      const totalDays = result.filter((stock) => stock.Ret).length
+      const totalDays = numberOfUpMoves + numberOfDownMoves
       const totalRetSum = result.reduce((acc, stock) => acc + (stock.Ret || 0.0), 0);
       const avgGain =
         numberOfUpMoves + numberOfDownMoves > 0
