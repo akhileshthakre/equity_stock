4deb56d398b2c61d4eda46b39da3fde74ce6c84b
Deepak <deepakadluri@gmail.com>
2024-04-24 12:55:53 +0530

    stock symbols changes deepak

--- app/src/app/core/header/header.component.ts
+++ app/src/app/core/header/header.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { switchMap } from 'rxjs';
 import { StocksApiService } from 'src/app/shared/apis/stocks.service';
 import { SpinnerService } from 'src/app/shared/spinner/spinner.service';
@@ -9,11 +10,14 @@ import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [MessageService],
 })
 export class HeaderComponent {
-  constructor(private _route: Router, private stocksApiService: StocksApiService, private spinnerService: SpinnerService) { }
+  constructor(private _route: Router, private messageService: MessageService, private stocksApiService: StocksApiService, private spinnerService: SpinnerService) { }
   visible: boolean = false;
+  successMessage: string = '';
+  failMessage: string = '';
   uploadStocks: any[] = []
   uploadedFiles: any[] = []
   datePipe: DatePipe = new DatePipe("en-US");
@@ -33,14 +37,30 @@ export class HeaderComponent {
   }
   onUploadStocks(event: any, fileUploadForStocks: any) {
     this.uploadedFiles = []
-    this.spinnerService.showSpinner(true)
     for (let file of event.files) {
       this.uploadedFiles.push(file);
     }
-    this.stocksApiService.uploadStocksSymbols(this.uploadedFiles).pipe(switchMap(() => this.stocksApiService.getAllStockSymbols())).subscribe((resp: any[]) => {
-      this.spinnerService.showSpinner(false)
-      if (resp) {
-        fileUploadForStocks.clear()
+    this.stocksApiService.uploadStocksSymbols(this.uploadedFiles).subscribe(resp => {
+      this.successMessage =''
+      fileUploadForStocks.clear()
+      if (resp['success'] == true) {
+        this.failMessage = ''
+        this.getStockSymbols();
+      } else {
+        this.failMessage = resp?.message
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: "failed" });
+      }
+    }, error => {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Upload Failed' });
+      fileUploadForStocks.clear()
+    })
+
+  }
+
+  getStockSymbols() {
+    this.stocksApiService.getAllStockSymbols().subscribe((resp) => {
+      this.successMessage =''
+      if (resp['success'] != "false") {
         resp.map((product: any) => {
           delete product.userId;
           product.name = String(product.name);
@@ -54,10 +74,13 @@ export class HeaderComponent {
         const wb: XLSX.WorkBook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
         XLSX.writeFile(wb, 'stock_symbols.xlsx');
+        this.successMessage = "Download successful" 
+      } else {
+        this.failMessage = resp?.message       
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Upload Failed' });
       }
-    },error=>{
-      this.spinnerService.showSpinner(false) 
-      fileUploadForStocks.clear()
+    }, error => { 
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Upload Failed' });
     })
 
   }
