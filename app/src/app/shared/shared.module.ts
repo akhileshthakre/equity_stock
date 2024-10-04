3116b62bb4620e2b48665a23431eaa4fb9e54b6c
AK <ameerkhan@epethilial.com>
2024-10-04 16:52:36 +0530

    Fixed data issues

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { UnauthorizedaccessComponent } from './components/unauthorizedaccess/unauthorizedaccess.component';
 import { TwoDigitDecimalDirective } from './directives/two-digit-decimal.directive';
+import { TwoDigitDecimalDirectiveWithNegative } from './directives/two-digit-decimal-with-negative.directive';
 
 @NgModule({
   declarations: [
     PagenotfoundComponent,
     UnauthorizedaccessComponent,
-    TwoDigitDecimalDirective
+    TwoDigitDecimalDirective,
+    TwoDigitDecimalDirectiveWithNegative
   ],
   imports: [
     CommonModule
   ],
   exports: [
     PagenotfoundComponent,
-    TwoDigitDecimalDirective
+    TwoDigitDecimalDirective,
+    TwoDigitDecimalDirectiveWithNegative
   ],
   
 })
